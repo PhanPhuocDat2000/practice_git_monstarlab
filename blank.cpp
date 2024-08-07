416c78738ccd42a7ab6a3cc1db2286e5b19e4021
PhanPhuocDat2000 <datphanphuoc2@gmail.com>
2024-08-07 13:32:27 +0700

    Resolve conflict

--- blank.cpp
+++ blank.cpp
@@ -4,14 +4,7 @@
 
 int main()
 {
-<<<<<<< HEAD
     cout << "ahihi" << endl;
 
     return 0;
 }
-=======
-  cout << "Lam cai gi do de tao conflict" << endl;
-  
-  return 0;
-}
->>>>>>> 633b976a301dca0134562ac22b0b5e9ee6bf4af3
